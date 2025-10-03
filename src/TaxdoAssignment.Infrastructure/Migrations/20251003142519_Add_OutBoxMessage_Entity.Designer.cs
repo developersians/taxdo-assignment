08c130ee41ffe2318ad7d98c5ad1dd8b1bed321c
developersians <developersian@gmail.com>
2025-10-03 17:56:35 +0330

    migrate: Apply migration

--- src/TaxdoAssignment.Infrastructure/Migrations/20251003142519_Add_OutBoxMessage_Entity.Designer.cs
+++ src/TaxdoAssignment.Infrastructure/Migrations/20251003142519_Add_OutBoxMessage_Entity.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaxdoAssignment.Infrastructure;
 
@@ -12,9 +13,11 @@ using TaxdoAssignment.Infrastructure;
 namespace TaxdoAssignment.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251003142519_Add_OutBoxMessage_Entity")]
+    partial class Add_OutBoxMessage_Entity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
